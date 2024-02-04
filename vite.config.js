a742a1d2fcc09e0936e97969d799a1a71d1a2fb9
Radif <59629676+Radifical@users.noreply.github.com>
2024-02-04 14:07:36 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 1100, // Adjust the limit as needed (in kB)
+  },
   plugins: [react()],
 })
